5ee15b8ad0c66ecfb4ee13737483a4dfc774a691
Gideon <33206022+GideonAgboba@users.noreply.github.com>
2018-02-13 03:22:10 -0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 	<?php
 /* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'accounts';
+$host = 'ec2-54-227-243-210.compute-1.amazonaws.com';
+$user = 'vjiqonnlaihdyi';
+$pass = 'b1e63e0bc10250947f197c9ddd13d781e74eac8922af0e46607af5f8a1fb0116';
+$db = 'd5urd1kbbaf3ti';
 $mysqli = new mysqli($host, $user, $pass, $db) or die($mysqli->error);
 ?>
